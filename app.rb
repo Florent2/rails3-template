003d07a2662673ade563a3341fb23e734fc73fc6
Florent Guilleux <florent2@gmail.com>
2010-09-17 11:28:30 -0500

    Remove rails.js before getting the jQuery version else get an prompt

--- app.rb
+++ app.rb
@@ -32,6 +32,7 @@ application generators
 
 get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js",  "public/javascripts/jquery.js"
 get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/jquery-ui.min.js", "public/javascripts/jquery-ui.js"
+remove_file "public/javascripts/rails.js"
 get "http://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
 
 jquery = <<-JQUERY
